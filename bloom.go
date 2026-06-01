789791f2ab0858c5252889bae23ea8f9cd19d6c3
Ilya Tribusean <slash3b@gmail.com>
2026-06-01 20:56:44 +0300

    xxhash seems to be much faster, also removed any allocations

--- bloom.go
+++ bloom.go
@@ -1,54 +1,49 @@
 package bloom
 
 import (
-	"crypto/md5"
-	"encoding/binary"
+	"github.com/OneOfOne/xxhash"
 )
 
 type Filter struct {
-	bits []byte
-	size int
+	bits    []byte
+	size    int
+	bitsize uint64
 }
 
 func New(s int) Filter {
 	return Filter{
-		bits: make([]byte, s),
-		size: s,
+		bits:    make([]byte, s),
+		size:    s,
+		bitsize: uint64(s * 8),
 	}
-}
-
-func (f *Filter) indexes(s string) []int {
-	res := md5.Sum([]byte(s))
-
-	bitsize := uint64(f.size * 8)
-
-	a := int(binary.LittleEndian.Uint64(res[0:md5.Size/2]) % bitsize)
-	bucketPos := a / 8
-	bitPos := a % 8
-
-	b := int(binary.LittleEndian.Uint64(res[md5.Size/2:]) % bitsize)
-	bucketPos2 := b / 8
-	bitPos2 := b % 8
-
-	return []int{bucketPos, bitPos, bucketPos2, bitPos2}
 }
 
 // Set adds entry into the Filter
 func (f *Filter) Set(s string) {
-	idxs := f.indexes(s)
+	h1 := xxhash.ChecksumString64S(s, 0)
+	h2 := xxhash.ChecksumString64S(s, 42)
 
-	f.bits[idxs[0]] |= 1 << idxs[1]
-	f.bits[idxs[2]] |= 1 << idxs[3]
+	a := int(h1 % f.bitsize)
+	b := int(h2 % f.bitsize)
+
+	f.bits[a/8] |= 1 << (a % 8)
+	f.bits[b/8] |= 1 << (b % 8)
 }
 
 // Get checks if element exists.
 func (f *Filter) Get(s string) bool {
-	idxs := f.indexes(s)
+	h1 := xxhash.ChecksumString64S(s, 0)
+	h2 := xxhash.ChecksumString64S(s, 42)
 
-	for i := 0; i < len(idxs); i = i + 2 {
-		if f.bits[idxs[i]]&(1<<idxs[i+1]) == 0 {
-			return false
-		}
+	a := int(h1 % f.bitsize)
+	b := int(h2 % f.bitsize)
+
+	if f.bits[a/8]&(1<<(a%8)) == 0 {
+		return false
+	}
+
+	if f.bits[b/8]&(1<<(b%8)) == 0 {
+		return false
 	}
 
 	return true
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bloom
 
 go 1.26.2
+
+require github.com/OneOfOne/xxhash v1.2.8
